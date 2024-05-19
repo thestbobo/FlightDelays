65f8e2e1e98d6dc218d3779244f394c157eb0875
Giuseppe Averta <g.averta3@gmail.com>
2024-05-19 17:10:57 +0200

    add airport dataclass and DAO method

--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,23 @@
 from database.DB_connect import DBConnect
+from model.airport import Airport
 
 
 class DAO():
-    def __init__(self):
-        pass
+
+    @staticmethod
+    def getAllAirports():
+        conn = DBConnect.get_connection()
+
+        result = []
+
+        cursor = conn.cursor(dictionary=True)
+        query = """SELECT * from airports a order by a.AIRPORT asc"""
+
+        cursor.execute(query)
+
+        for row in cursor:
+            result.append(Airport(**row))
+
+        cursor.close()
+        conn.close()
+        return result
